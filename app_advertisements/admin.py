2b2bb2c314af2f050a4636b4ab381085d7f88866
pavel <pavel@mail.ru>
2023-08-03 16:36:20 +0300

    7.0

--- app_advertisements/admin.py
+++ app_advertisements/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Advertisements # импортирую свою модель
+# py manage.py createsuperuser - создания аккаунта супер пользователя
+# пароль не отображается
+# http://127.0.0.1:8000/admin
+
+# кастомный класс для модели
+class AdvertisementsAdmin(admin.ModelAdmin):
+    list_display  = ['id', 'title','description','price','auction','created_at'] # какие поля должны отображатья в таблице
 
 
+
+# регистрирую модели и 
+admin.site.register(Advertisements, AdvertisementsAdmin)
--- app_advertisements/models.py
+++ app_advertisements/models.py
@@ -10,27 +10,23 @@ class Cats(models.Model):
     breed = models.CharField(max_length=50)
 
 
-
 # главный
 
-
+# venv/Scripts/activate   
+# py manage.py makemigrations
+# py manage.py migrate
 # заголовок - описание - цена - дата создания - дата обновления - тогр
 
 
 class Advertisements(models.Model):
     title = models.CharField('заголовок',max_length=100)
-    descriptoin = models.TextField('описание')
+    description = models.TextField('описание')
     price = models.DecimalField('цена',max_digits=10, decimal_places=2)
     auction = models.BooleanField("торг", help_text='Возможен торг или нет',default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
     
-    
-
-
-
-
-
+  
     # представление в виде строки 
     def __str__(self) -> str:
         return f"Advertisements(id = {self.id}, title = {self.title}, price = {self.price})"
