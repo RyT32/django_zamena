fcb713ef9e868f60cbe5a807c85bbfacae7cf0bd
pavel <pavel@mail.ru>
2023-08-03 17:25:38 +0300

    8.0

--- app_advertisements/admin.py
+++ app_advertisements/admin.py
@@ -9,9 +9,26 @@ from django.db.models.query import QuerySet
 
 # кастомный класс для модели
 class AdvertisementsAdmin(admin.ModelAdmin):
-    list_display  = ['id', 'title','description','price','auction','created_at'] # какие поля должны отображатья в таблице
+    list_display  = ['id', 'title','description','price','auction', 'created_date', 'update_date'] # какие поля должны отображатья в таблице
     list_filter = ['auction', 'created_at'] # поля по которыфм  можно  сделать фильтрацию
     actions = ['make_auction_as_false','make_auction_as_true'] # спискок функций для выбранных элементов
+    fieldsets = ( # указание разделов для полей записи
+        ('Общее', { # название раздела 1
+            "fields": (
+                'title','description' # поля раздела
+            ),
+        }),
+        ('Фианансы', { # название раздела 2
+            "fields": (
+                'price','auction'
+            ),
+            'classes':['collapse'] # функция скрытия раздела
+        }),
+    )
+    
+
+
+
 
 
     # функции для выбранных элементов
--- app_advertisements/models.py
+++ app_advertisements/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+
+        # модули для настройки методов админки
 from django.contrib import admin
-from django.utils import timezone
-from django.utils.html import format_html
+from django.utils import timezone # для времени
+from django.utils.html import format_html # для создания строки html
+        #
+
 # Create your models here.
 # тестовый класс
 class Cats(models.Model):
@@ -34,6 +38,34 @@ class Advertisements(models.Model):
     # настройки для таблицы
     class Meta:
         db_table = 'advertisements' # переименовали таблицу 
+
+    #если запись была создана сегодня то она будет зеленого цвета
+    @admin.display(description='дата создания')
+    def created_date(self):
+        if self.created_at.date() == timezone.now().date():#проверяю что запись была создана сегодня
+            created_time = self.created_at.time().strftime('%H:%M:%S') #формат 17:14:25
+            return format_html(
+                '<span style="color:green; font-weight:bold">сегодня в {}</span>',created_time
+            )
+        return self.created_at.strftime('%d.%m.%Y at %H:%M:%S') #формат 03.08.2023 at 17:14:25
+
+
+
+    #если запись была создана сегодня то она будет зеленого цвета
+    @admin.display(description='дата обновления')
+    def update_date(self):
+        if self.update_at.date() == timezone.now().date():#проверяю что запись была создана сегодня
+            update_time = self.update_at.time().strftime('%H:%M:%S') #формат 17:14:25
+            return format_html(
+                '<span style="color:green; font-weight:bold">сегодня в {}</span>',update_time
+            )
+        return self.update_at.strftime('%d.%m.%Y at %H:%M:%S') #формат 03.08.2023 at 17:14:25
+
+
+
+
+
+
 
 
 
